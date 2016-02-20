36e7c44c0f16bfab739a8fa00d0b5f2269e5dc5f
yadianna13 <yadianna13@gmail.com>
2016-02-20 11:49:17 +0200

    changes to it_company

--- it_company.rb
+++ it_company.rb
@@ -9,11 +9,12 @@ class Developer
   end
 
   def add_task(task_name)
-    if @task_list.count >= self.class::MAX_TASKS
+    if @task_list.count == self.class::MAX_TASKS
       raise("Слишком много работы!") 
     else
-      @task_list.push(task_name)
-      puts "#{@dev_name}: добавлена задача \"#{task_name}\". Всего в списке задач: #{@task_list.count}"
+      @task_list << task_name
+      puts "%s: добавлена задача \"%s\". Всего в списке задач: %i" %
+        [@dev_name, task_name, @task_list.count]
     end
   end
 
@@ -22,18 +23,12 @@ class Developer
   end
 
   def work!
-    if @task_list.count <= 0
+    if @task_list.empty?
        raise("Нечего делать!")
     else
-      puts "#{@dev_name}: #{self.class::WORK_DONE} \"#{@task_list.shift}\". Осталось задач: #{@task_list.count}"
+      puts "%s: %s \"%s\". Осталось задач: %i" %
+        [@dev_name, self.class::WORK_DONE, @task_list.shift, @task_list.count]
     end
-  end
-
-  def status
-    return "свободен" if @task_list.count.zero?
-    return "занят" if @task_list.count == self.class::MAX_TASKS
-    return "работаю"
-    
   end
 
   def can_add_task?
@@ -43,31 +38,49 @@ class Developer
   def can_work?
     @task_list.count > 0 ? true : false
   end
+
+    def status
+    case 
+      when !can_add_task?
+        "занят" 
+      when can_work?
+        "работаю" 
+      else
+        "свободен"  
+    end
+  end
+
 end
+
+#------------------------------------------------------------------------------
 
 class JuniorDeveloper < Developer
   attr_accessor :dev_name
 
   MAX_TASKS = 5
   WORK_DONE = "пытаюсь делать задачу"
-
+  COMPLEXITY = 20
 
   def add_task(task_name)
-    raise("Слишком сложно!") if task_name.length > 20
+    raise("Слишком сложно!") if task_name.length > COMPLEXITY
     super
   end
 end
+
+#------------------------------------------------------------------------------
 
 class SeniorDeveloper < Developer
   
   MAX_TASKS = 15
 
   def work!
-    good_mood = [true, false][rand(0..1)]
-    if good_mood 
-      @task_list.shift(2)
-    else
-      puts "Что-то лень"
-    end
+    [true, false].sample ? @task_list.shift(2) : puts("Что-то лень")
+#    good_mood = [true, false].sample
+#    if good_mood 
+#      @task_list.shift(2)
+#    else
+#      puts "Что-то лень"
+#    end
   end
 end
+
